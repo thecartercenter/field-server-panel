b483e97e64ff7935663aa10f8cc6419e972b1e93
Tom Smyth <tom@sassafras.coop>
2020-01-31 15:57:12 -0500

    Disable caching

--- app.rb
+++ app.rb
@@ -15,6 +15,11 @@ helpers do
   alias_method :h, :escape_html
 end
 
+before do
+  # We never want to cache any of our pages.
+  expires(0, :no_store, :no_cache, :must_revalidate)
+end
+
 get "/" do
   erb(:main)
 end
